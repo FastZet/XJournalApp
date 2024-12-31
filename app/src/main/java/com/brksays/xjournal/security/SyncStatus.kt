8eef98a10ad58fde340c5253162565e75244049e
Roskova Marcoraz <bjrk1992@gmail.com>
2025-01-01 01:01:10 +0530

    Delete app/src/main/java/com/brksays/xjournal/security/SyncStatus.kt
    duplicated in journalentry

--- app/src/main/java/com/brksays/xjournal/security/SyncStatus.kt
+++
@@ -1,8 +0,0 @@
-package com.brksays.xjournal.security
-
-enum class SyncStatus {
-    SYNCED,        // Entry is synchronized with cloud storage
-    NOT_SYNCED,    // Entry needs to be synchronized
-    PENDING_SYNC,  // Sync is in progress
-    SYNC_ERROR     // Error occurred during synchronization
-}
